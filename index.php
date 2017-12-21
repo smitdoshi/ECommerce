ae8dd324dda8c98af8877ee5cd3bb422f12b3c5a
SMIT DOSHI <smitdoshi007@gmail.com>
2017-12-21 07:40:21 -0500

    Added Bootstrap and File Stuct

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SMITDOSHI
+ * Date: 12/21/17
+ * Time: 7:29 AM
+ */
+
+?>
+
+
+<!DOCTYPE html>
+<!--[if IE 8 ]><html class="no-js oldie ie8" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="no-js oldie ie9" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en"> <!--<![endif]-->
+
+    <head>
+
+        <!--- basic page needs
+       ================================================== -->
+        <meta charset="utf-8">
+        <title>E Commerce </title>
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <!-- mobile specific metas
+        ================================================== -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+        <!--- Meta tag to referesh the page every 2 seconds-->
+        <!--    ================================================== -->
+        <!--    <meta http-equiv="refresh", content="2">-->
+
+        <!--- Linking Bootstrap
+       ================================================== -->
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="style/bootstrap.min.css">
+
+    </head>
+
+    <body>
+
+
+
+        <!--- Linking Bootstrap Javascript File
+          ================================================== -->
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    
+    </body>
+
+</html>
