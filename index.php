4665fd678ecdf95d7ee0349d48792753ba293221
SMIT DOSHI <smitdoshi007@gmail.com>
2017-12-23 14:25:41 -0500

    Added Navbar Header

--- index.php
+++ index.php
@@ -44,10 +44,170 @@
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
 
+        <!--- Linking CSS Styling Sheet
+        ================================================== -->
+
+        <link rel="stylesheet" href="style/index.css">
+
     </head>
 
     <body>
 
+        <div id="top"> <!-- Top Start -->
+
+            <div class="container"> <!-- Container Start -->
+
+                <div class="col-md-6 offer"> <!-- col-md-6 offer Start -->
+
+                    <a href="#" class="btn btn-success btn-sm">
+                        Welcome: Guest
+                    </a>
+
+                    <a href="#">
+                        Shopping Cart Total Price: $100, Total items 2
+                    </a>
+                </div> <!-- col-md-6 offer End -->
+
+                <div class="col-md-6"> <!-- col-md-6 Start -->
+
+                    <ul class="menu"> <!-- menu Starts -->
+
+                        <li>
+                            <a href="customer_register.php">
+                                Register
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="checkout.php">
+                                My Account
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="cart.php">
+                                Go to Cart
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="checout.php">
+                                Login
+                            </a>
+                        </li>
+
+                    </ul> <!-- menu Ends -->
+
+                </div> <!-- col-md-6 End -->
+
+            </div> <!-- Container Close -->
+
+        </div> <!-- Top END -->
+
+        <div class="navbar navbar-default" id="navbar"> <!-- navbar navbar-default Start -->
+
+            <div class="container"> <!-- container Starts -->
+
+                <div class="navbar-header"> <!-- navbar-header Start -->
+
+                    <a class="navbar-brand home" href="index.php"> <!-- navbar-brand home Start -->
+
+                        <img src="#" alt="Compnay Logo" class="hidden-xs">
+                        <img src="#" alt="Company Logo" class="visible-xs">
+
+                    </a> <!-- navbar-brand home Ends -->
+
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+
+                        <span class="sr-only">Toogle Navigation </span>
+
+                        <i class="fa fa-align-justify"></i>
+
+                    </button>
+
+                    <button type="button" class="navbar-toggle" data-toggle="navbar-toggle" data-target="#search">
+                        <span class="sr-only">Toggle Search</span>
+
+                        <i class="fa fa-search"></i>
+                    </button>
+
+                </div> <!-- navbar-header End -->
+
+                <div class="navbar-collapse collapse" id="navigation"> <!-- navbar-collapse collapse Starts -->
+
+                    <div class="padding-nav"> <!-- Padding Nav Starts -->
+
+                        <ul class="nav navbar-nav navbar-left"> <!-- nav navbar-nav navbar-left Starts -->
+
+                            <li class="active">
+                                <a href="index.php"> Home </a>
+                            </li>
+
+                            <li>
+                               <a href="shop.php"> Shop</a>
+                            </li>
+
+                            <li>
+                                <a href="checkout.php"> My Account</a>
+                            </li>
+
+                            <li>
+                                <a href="cart.php"> Shopping Cart</a>
+                            </li>
+
+                            <li>
+                                <a href="contact.php">Contact Us</a>
+                            </li>
+
+                        </ul> <!-- nav navbar-nav navbar-left Ends -->
+
+                    </div> <!-- Padding Nav Ends -->
+
+                    <a class="btn btn-primary navbar-btn right" href="cart.php"> <!-- btn btn-primary navbar-btn right Starts -->
+
+                        <i class="fa fa-shopping-cart"></i>
+
+                        <span>  4 items in cart </span>
+
+                    </a> <!-- btn btn-primary navbar-btn Ends -->
+
+                    <div class="navbar-collapse collapse right"> <!-- navbar-collapse collapse right Starts -->
+
+                        <button class="btn navbar-btn btn-primary" type="button" data-toggle="collapse" data-target="#search">
+
+                            <span class="sr-only">Toggle Search</span>
+
+                            <i class="fa fa-search"></i>
+
+                        </button>
+
+                    </div> <!-- navbar-collapse collapse right Ends -->
+
+                    <div class="collapse clearfix" id="search"> <!-- collapse clearfix Starts -->
+
+                        <form class="navbar-form" method="get" action="results.php"> <!-- navbar-form Starts -->
+
+                            <div class="input-group"> <!-- input-group Starts -->
+
+                                <input class="form-control" type="text" placeholder="Search" name="user_query" required>
+
+                                <button type="submit" value="Search" name="search" class="btn btn-primary">
+
+                                    <i class="fa fa-search"></i>
+
+                                </button>
+
+                            </div> <!-- input-group Ends -->
+
+                        </form> <!-- navbar-form Ends -->
+
+                    </div> <!-- collapse clearfix Ends -->
+
+                </div> <!-- navbar-collapse collapse Ends -->
+
+            </div> <!-- container Ends -->
+
+        </div> <!-- navbar navbar-default Ends -->
 
 
 
@@ -59,7 +219,6 @@
         <!--- Linking Javascript File
           ================================================== -->
         <script type="text/javascript" src="js/bootstrap.min.js"></script>
-
     </body>
 
 </html>
