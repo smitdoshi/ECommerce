200f48bf7f44a6db6b58065935bcee42ee12426d
SMIT DOSHI <smitdoshi007@gmail.com>
2017-12-24 10:28:29 -0500

    Added Contents to Footer

--- includes/footer.php
+++ includes/footer.php
@@ -40,6 +40,72 @@
                 <hr class="hidden-md hidden-lg hidden-sm">
 
             </div> <!-- col-md-3 col-sm-6 Ends -->
+
+            <div class="col-md-3 col-sm-6"> <!-- col-md-3 col-sm-6 Starts -->
+
+                <h4> Top Product Categories</h4>
+
+                <ul> <!-- ul Starts -->
+
+                    <li> <a href="#">Jacktes</a></li>
+                    <li> <a href="#">Accessories</a></li>
+                    <li> <a href="#">Shoes</a></li>
+                    <li> <a href="#">Coats</a></li>
+                    <li> <a href="#">T-Shirts</a></li>
+
+                </ul> <!-- ul Ends -->
+
+                <hr class="hidden-md hidden-lg">
+
+            </div> <!-- col-md-3 col-sm-6 Ends -->
+
+            <div class="col-md-3 col-sm-6"> <!-- col-md-3 col-sm-6 Starts -->
+
+                <h4> Where to find us</h4>
+
+                <p> <!-- p Starts -->
+
+                    <strong>Company Name</strong>
+                    <br>Address Line1
+                    <br>City Name
+                    <br>State
+                    <br>PostalCode
+                    <br> Phone Number
+                    <br>Email Address
+                    <br>
+
+                    <strong> Person's Name </strong>
+
+                </p> <!-- p Ends -->
+
+                <a href="contact.php">Go to Contact us page</a>
+
+                <hr class="hidden-md hidden-lg">
+            </div> <!-- col-md-3 col-sm-6 Ends -->
+
+            <div class="col-md-3 col-sm-6"> <!-- col-md-3 col-sm-6 Starts -->
+
+                <h4>Get the News </h4>
+
+                <p class="text-muted">
+                    Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content
+
+                </p>
+
+                <form action="" method="post"> <!-- form Starts -->
+
+                    <div class="input-group"> <!-- input-group Starts -->
+
+                        <input type="text" class="form-control" name="email">
+
+                        <span class="input-group-btn"> <!-- input-group-btn Starts -->
+
+                            <input type="submit" value="subscribe" class="btn btn-default">
+
+                        </span> <!-- input-group-btn Ends -->
+                    </div> <!-- input-group Ends -->
+                </form> <!-- form Ends -->
+            </div> <!-- col-md-3 col-sm-6 Ends -->
         </div> <!-- row Ends -->
     </div> <!-- container Ends -->
 </div> <!-- footer Ends -->
